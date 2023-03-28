c9f08e4326c059afb268dea78b339bebbc82a4e4
schristinelee <christinelee1527@gmail.com>
2023-03-28 02:34:05 -0700

    added values of the keys

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 
 const App = () => {
   const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+    console.log(e.target.value);
+  };
   return (
     <>
       <div className="container">
@@ -10,24 +15,57 @@ const App = () => {
         </form>
 
         <div className="keypad">
-          <button>AC</button>
-          <button>C</button>
-          <button>&divide;</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>&times;</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>&ndash;</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button onClick={handleClick} value={"AC"} id="clear">
+            AC
+          </button>
+          <button onClick={handleClick} value={"C"} id="backspace">
+            C
+          </button>
+          <button onClick={handleClick} value={"/"} type="button" className="btn btn-primary">
+            &divide;
+          </button>
+          <button onClick={handleClick} value={"x"}>
+            &times;
+          </button>
+          <button onClick={handleClick} value={"-"}>
+            &ndash;
+          </button>
+          <button onClick={handleClick} value={"="}>
+            =
+          </button>
+          <button onClick={handleClick} value={"+"}>
+            +
+          </button>
+          <button onClick={handleClick} value={9}>
+            9
+          </button>
+          <button onClick={handleClick} value={8}>
+            8
+          </button>
+          <button onClick={handleClick} value={7}>
+            7
+          </button>
+          <button onClick={handleClick} value={6}>
+            6
+          </button>
+          <button onClick={handleClick} value={5}>
+            5
+          </button>
+          <button onClick={handleClick} value={4}>
+            4
+          </button>
+          <button onClick={handleClick} value={3}>
+            3
+          </button>
+          <button onClick={handleClick} value={2}>
+            2
+          </button>
+          <button onClick={handleClick} value={1}>
+            1
+          </button>
+          <button onClick={handleClick} value={0}>
+            0
+          </button>
         </div>
       </div>
     </>
